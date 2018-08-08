e05cbcf2ae37503edc9cfb80af534c2ea9402a63
EslavaDev <danieleslava52@gmail.com>
2018-08-08 10:01:23 -0500

    logo

--- sql/src/App.js
+++ sql/src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
       <div className="App">
       
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo}  className="App-logo" alt="logo" />
           <Form inline>
           <FormGroup>
           <Label for="exampleEmail" hidden>Db</Label>
