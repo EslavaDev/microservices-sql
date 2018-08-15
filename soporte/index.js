01c0b9a620194ea6c13135ca67b7387e7fd00d44
EslavaDev <danieleslava52@gmail.com>
2018-08-15 16:43:44 -0500

    server

--- soporte/index.js
+++ soporte/index.js
@@ -5,7 +5,7 @@ seneca.use(require('./src/actions'));
 //correr por socket
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://10.75.58.6:27017/sql', (err, res) =>{
+mongoose.connect('mongodb://127.0.0.1:27017/sql', (err, res) =>{
 	if(err){
 		throw err;
 	}else{
