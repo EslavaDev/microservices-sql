93fd779a2eb89b0e75d67064e431c0767285041e
EslavaDev <danieleslava52@gmail.com>
2018-08-16 10:18:05 -0500

    server

--- soporte/index.js
+++ soporte/index.js
@@ -5,12 +5,17 @@ seneca.use(require('./src/actions'));
 //correr por socket
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://31.220.55.37:27017/sql', (err, res) =>{
-	if(err){
-		throw err;
-	}else{
-		console.log("Base de datos Conectada Correctamente");
-        seneca.listen({port: 10002, type: "tcp", pin:"role:Soporte"});
-	}
-});
+try{
+	mongoose.connect('mongodb://31.220.55.37:27017/sql', (err, res) =>{
+		if(err){
+			throw err;
+		}else{
+			console.log("Base de datos Conectada Correctamente");
+			seneca.listen({port: 10002, type: "tcp", pin:"role:Soporte"});
+		}
+	});
+
+}catch(err){
+	console.error(err);
+}
 
