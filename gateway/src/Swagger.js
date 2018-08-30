6cb5a49e05e6788cb9e21c1040c92e052b6cc0c0
EslavaDev <danieleslava52@gmail.com>
2018-08-30 15:18:28 -0500

    excel basic

--- gateway/src/Swagger.js
+++ gateway/src/Swagger.js
@@ -1,24 +1,24 @@
 const pkg = require('../package.json');
-module.exports= {
-    documentationPath: '/',
-    basePath: '/api/',
-    host:'186.147.235.10:64444', 
-    info: {
-      title: 'Sql Api',
-      description: 'microservices Sql API',
-      version: pkg.version,
-      contact: {
-        name: 'Daniel Eslava',
-        email: 'danieleslava52@gmail.com'
-      }
-    },
-    securityDefinitions: {
-      'jwt': {
-        'type': 'apiKey',
-        'name': 'Authorization',
-        'in': 'header'
-      }
-    },
-    grouping: 'tags',
-    sortEndpoints: 'ordered'
-  }
+module.exports = {
+  documentationPath: '/',
+  basePath: '/api/',
+  host: '192.168.1.10:64444',
+  info: {
+    title: 'Sql Api',
+    description: 'microservices Sql API',
+    version: pkg.version,
+    contact: {
+      name: 'Daniel Eslava',
+      email: 'danieleslava52@gmail.com'
+    }
+  },
+  securityDefinitions: {
+    'jwt': {
+      'type': 'apiKey',
+      'name': 'Authorization',
+      'in': 'header'
+    }
+  },
+  grouping: 'tags',
+  sortEndpoints: 'ordered'
+}
